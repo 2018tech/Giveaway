b1a4fbc24a05fe09eac26e73f2d9c0049f961c83
Jongwon Lee <jlee@colby.edu>
2018-08-10 16:34:03 -0700

    mainserverback

---
+++ src/postloggedin/main.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import ReactMapboxGl, { Feature,Layer, Popup } from "react-mapbox-gl";
+
+const Map = ReactMapboxGl({
+  accessToken: "pk.eyJ1IjoiamxlZWVpIiwiYSI6ImNqa2JmdnBydzBqcXkzb3A4MXR3aGZ5M3IifQ.1BYG_EtmKkW8YTeBYwcL9A"
+});
+
+export default class Mainpage extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      region: {
+        latitude: 0,
+        longitude: 0
+      },
+      showmap: false,
+      showPopup: false,
+      stations: {},
+      station: {}
+    }
+  }
+
+  markerClick(station) {
+    console.log(station)
+    this.setState({
+      region: {
+        latitude: station.position[1],
+        longitude: station.position[0]
+      },
+      station: station,
+      showPopup: true
+    })
+  }
+
+  onCancel(e) {
+    this.setState({
+      showPopup: false
+    });
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        this.setState({region: {
+          latitude: success.coords.latitude,
+          longitude: success.coords.longitude
+        }, showmap: true
+      });
+    },
+    (error) => {
+      console.log(error)
+    }
+  )
+  fetch('/usershop', {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res=> res.json())
+  .then(res=> {
+    var obj = {}
+    res.map(item=> {
+      obj[item["_id"]]={position: item.locations.position, yourshopname: item.locations.yourshopname, items: item.items}}
+    )
+
+    this.setState({stations: obj})
+  })
+  .catch(err=> console.log(err))
+}
+
+render() {
+  return(
+    <div className="home"> {this.state.showmap &&
+      <Map
+        container='map'
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw"
+        }}
+        center={[this.state.region.longitude, this.state.region.latitude]}
+        zoom={[15]}
+        >
+          <Layer
+            type="symbol"
+            layout={{ "icon-image": "harbor-15" }}>
+            {Object.keys(this.state.stations).map((stationkey, index) => {
+              return (<Feature
+                onClick={this.markerClick.bind(this, this.state.stations[stationkey])}
+                coordinates={this.state.stations[stationkey]["position"]}
+              />)
+            })}
+          </Layer>
+          {this.state.showPopup ?
+            <Popup
+              coordinates={this.state.station["position"]}
+              >
+                <div className="shopname">
+                  Welcome to {this.state.station.yourshopname}
+                  <div className="item">
+                    {this.state.station["items"].map(item=> <p>Name: {[item["name"]]}<br></br> Description: {item[["description"]]}<br></br> Value: {item[["value"]]}<button>Request</button></p>                )}
+                  </div>
+                  <div id="item">
+                    <button onClick={e => this.onCancel(e)}>Cancel</button>
+                  </div>
+                </div>
+              </Popup>: null}
+            </Map>
+          }</div>
+        )}
+      }
